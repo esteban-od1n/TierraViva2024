8cc40a95c2894032cf75d1fbd18057038b176493
Laura Castillo <lcastillo13@ucol.mx>
2024-09-29 15:53:54 -0600

    foro

---
+++ src/Controller/forum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class Foro extends AbstractController {
+
+    #[Route(path: '/foro', name: 'foro')]
+    public function content(): Response {
+        return $this->render('pages/foro.html.twig');
+    }
+
+}
+
