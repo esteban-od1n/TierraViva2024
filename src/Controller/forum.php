cab283c5552e8426947d64b2fe1b9dc444d25376
Laura Castillo <lcastillo13@ucol.mx>
2024-09-29 21:18:27 -0600

    medio foro

--- src/Controller/forum.php
+++ src/Controller/forum.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class Foro extends AbstractController {
+class forum extends AbstractController {
 
     #[Route(path: '/foro', name: 'foro')]
     public function content(): Response {
