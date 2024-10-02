c4d5becac6f1e019db1cc5391e8dfb37f996d1da
estebam-od1n <lalcaraz11@ucol.mx>
2024-10-02 14:48:01 -0600

    vista de admin

---
+++ src/Controller/adminFormVideos.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class adminFormVideos extends AbstractController {
+
+    #[Route(path: '/adminFormVideos', name: 'adminFormVideos')]
+    public function content(): Response {
+        return $this->render('pages/adminFormVideos.html.twig');
+    }
+}
