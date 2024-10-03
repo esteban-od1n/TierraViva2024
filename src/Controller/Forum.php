c14e775dbdf041f4c7a25eec0e8d7e30c3839ced
Mauricio <mcardenas@ucol.mx>
2024-10-03 10:24:37 -0600

    eliminar tribilin

--- src/Controller/Forum.php
+++ src/Controller/Forum.php
@@ -6,12 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class forum extends AbstractController {
-
-    #[Route(path: '/foro', name: 'foro')]
-    public function content(): Response {
-        return $this->render('pages/foro.html.twig');
+class Forum extends AbstractController
+{
+    #[Route(path: "/foro", name: "foro")]
+    public function content(): Response
+    {
+        return $this->render("pages/foro.html.twig");
     }
-
 }
-
