81dc0f27fb67fa30e38da697fc277f2ca906c520
Mauricio <mcardenas@ucol.mx>
2024-10-03 08:57:06 -0600

    feat(admin/users): Finished users

--- src/Controller/Profile.php
+++ src/Controller/Profile.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class profile extends AbstractController {
-
-    #[Route(path: '/profile', name: 'profile')]
-    public function content(): Response {
-        return $this->render('pages/profile.html.twig');
+class Profile extends AbstractController
+{
+    #[Route(path: "/perfil", name: "profile")]
+    public function content(): Response
+    {
+        return $this->render("pages/profile.html.twig");
     }
-
 }
