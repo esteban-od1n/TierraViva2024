bdf5c5a13131783d5eeed55b18b376a9f5d0f272
Mauricio <mcardenas@ucol.mx>
2024-12-02 22:38:21 -0600

    provider

--- src/Controller/ProvidersController.php
+++ src/Controller/ProvidersController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProviderCategoriesRepository;
 use App\Repository\ProviderRepository;
+use App\Repository\ProductRepository;
 
 class ProvidersController extends AbstractController
 {
@@ -15,16 +16,20 @@ class ProvidersController extends AbstractController
     public function index(
         #[MapQueryParameter] ?string $type = null,
         ProviderCategoriesRepository $cat_repo,
-        ProviderRepository $repo
+        ProductRepository $prod_repo
     ): Response {
         $categories = $cat_repo->findAll();
-        $providerQuery = $repo->createQueryBuilder("prov");
+        $providerQuery = $prod_repo->createQueryBuilder("p");
+        $providerQuery;
         if ($type) {
-            $providerQuery->andWhere($providerQuery->expr()->eq("", ""));
+            $providerQuery->andWhere(
+                $providerQuery->expr()->eq("p.provider.categories.name", $type)
+            );
         }
 
         return $this->render("providers/index.html.twig", [
             "categories" => $categories,
+            "products" => $providerQuery->getQuery()->execute(),
         ]);
     }
 }
